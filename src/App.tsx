e8504e6fb93e43bb62831efad6cc4017f85f13f3
Nakry Mut <nakrymut375@gmail.com>
2025-01-06 13:08:19 +0700

    first init

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+// src/App.tsx
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes ,Navigate} from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import HomePage from './View/HomePage';
+import Login from './components/Login'
+import Register from './components/Register';
+
+
+const App: React.FC = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    setIsAuthenticated(!!token);
+  }, []);
+
+  return (
+    <Router>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={isAuthenticated ? <Navigate to="/" /> : <Login />} />
+      </Routes>
+      <ToastContainer />
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/services/api.tsx
@@ -0,0 +1,3 @@
+
+
+export const API_URL = 'https://backend-mongodb-qioa.onrender.com/';
