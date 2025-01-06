56277645b46835ee07022be51c051c6f32a8ecd3
Nakry Mut <nakrymut375@gmail.com>
2025-01-06 14:17:49 +0700

    add port 5000

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+
+export default {
+    server: {
+      host: '0.0.0.0', // Ensures the server listens on all network interfaces
+      port: process.env.PORT || 3000, // Use the port provided by Render or default to 3000
+    },
+  };
